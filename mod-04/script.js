1aecc23c74bfe04dfa4d6d3008fc5e189de1e926
mucadlucca <mucael@gmail.com>
2020-06-12 13:57:11 -0300

    refatorado conforme RocketSeat

--- mod-04/script.js
+++ mod-04/script.js
@@ -5,23 +5,29 @@
 
 function checaIdade(idade) {
   return new Promise(function(resolve, reject) {   
-    var maiorIdade = function() {
-      if ( idade > 18) {       
-          resolve('Maior que 18');
-        } else {
-          reject('Menor que 18')
-        }
-      }
-    setTimeout(maiorIdade, 2000);
+    // var maiorIdade = function() {
+    //   if ( idade > 18) {       
+    //       resolve('Maior que 18');
+    //     } else {
+    //       reject('Menor que 18')
+    //     }
+    //   }
+    // setTimeout(maiorIdade, 2000);
+    
+    setTimeout(function() {
+      return idade > 18 ? resolve() : reject();
+    }, 2000)
   });
  }
 
  checaIdade(20)
   .then(function(response) {
-  console.log(response);
+    console.log('Maior que 18');
+  // console.log(response);
   })
   .catch(function(error) {
-  console.log(error);
+    console.log('Menor que 18')
+  // console.log(error);
   });
 //===========================================================================
 
@@ -32,53 +38,55 @@ function checaIdade(idade) {
 //URL de exemplo: https://api.github.com/users/diego3g/repos
 //Basta alterar "diego3g" pelo nome do usuário.
 
-var container = document.querySelector('#container')
-var inputElement = document.querySelector('input')
-var btnElement = document.querySelector('button')
+var container = document.querySelector('#container');
+var inputElement = document.querySelector('input');
+var btnElement = document.querySelector('button');
 
-btnElement.onclick = function() { 
-  var listElement = document.querySelector('#container ul')
+btnElement.onclick = function() {   
+  renderListElement();
+  var githubUser = inputElement.value; 
+
+  if (!githubUser) {
+    alert('Digite um usuário do github');
+    return
+  }  
+
+  renderLoading();
+
+  axios.get(`https://api.github.com/users/${githubUser}/repos`)
+  .then(function(response) {    
+    renderRepositories(response.data);    
+  })
+  .catch(function(error) {    
+    renderError();
+  }) 
+}
+
+function renderListElement() {
+  var listElement = document.querySelector('#container ul');
 
   if (!listElement) {
-    var listElement = document.createElement('ul')
+    var listElement = document.createElement('ul');
+    container.appendChild(listElement);
   } else {
     listElement.innerHTML = '';
   }
-     
-  var githubUser = inputElement.value  
-
-  if (!githubUser) {
-    alert('Digite um usuário do github')
-    return
-  }
-
-  var txtLoading = document.createTextNode('Carregando...') // Exercicio 3
-  var loadingElement = document.createElement('li')  // Exercicio 3
-  
-  loadingElement.appendChild(txtLoading)  // Exercicio 3
-  listElement.appendChild(loadingElement)  // Exercicio 3
-
-  axios.get(`https://api.github.com/users/${githubUser}/repos`)
-  .then(function(response) {
-    var repos = response.data 
-
-    listElement.innerHTML = '';
-
-    container.appendChild(listElement)
-
-    for (repo of repos) {      
-      var repoElement = document.createElement('li')
-      var repoName = document.createTextNode(repo.name)
-
-      repoElement.appendChild(repoName)
-      listElement.appendChild(repoElement)
-    }
-  })
-  .catch(function(error) {
-    listElement.innerHTML = ''; // Exercicio 3
-    alert('Erro: usuário não encontrado')  // Exercicio 3  
-  })
 }
+
+function renderRepositories(repos) {
+  var listElement = document.querySelector('#container ul');
+  listElement.innerHTML = '';  
+
+  for (repo of repos) {      
+    var repoElement = document.createElement('li');
+    var repoName = document.createTextNode(repo.name);
+
+    repoElement.appendChild(repoName);
+    repoElement.classList.add('repositorios');
+    listElement.appendChild(repoElement);
+  } 
+}
+
 //=====================================================================
 
 // Exercicio 3
@@ -88,6 +96,29 @@ btnElement.onclick = function() {
 //Além disso, adicione uma mensagem de erro em tela caso o usuário no Github não exista.
 //Dica: Quando o usuário não existe, a requisição irá cair no .catch com código de erro 404
 
+function renderLoading() {  
+  var listElement = document.querySelector('#container ul');
+  listElement.innerHTML = '';
 
+  var txtLoading = document.createTextNode('Carregando...');
+  var loadingElement = document.createElement('li'); 
+  
+  loadingElement.appendChild(txtLoading);
+  loadingElement.classList.add('loading'); 
+  listElement.appendChild(loadingElement); 
+}
 
+function renderError() { 
+  var listElement = document.querySelector('#container ul');
+  listElement.innerHTML = '';
+
+  var txtError = document.createTextNode('Erro! Usuário não encontrado');
+  var errorElement = document.createElement('li'); 
+
+  errorElement.style.color = '#F00';
+  
+  errorElement.appendChild(txtError);
+  errorElement.classList.add('error');
+  listElement.appendChild(errorElement);
+}
  
